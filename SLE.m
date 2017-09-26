d2fe7404b273e765743f7787b6de33b193b765ee
Said Boutiche <32273520+xsources@users.noreply.github.com>
2017-09-26 16:36:11 +0200

    SLE.m
    Main SLE script

---
+++ SLE.m
@@ -0,0 +1,40 @@
+function [y,x]=SLE(n,chi)
+%---------------------------------------------------
+% Schramm Lowener Evolution (Matlab simulation of stochastic planar curves)
+% author : Said Boutiche
+% email : myelearningz@gmail.com
+% date : 2017 Sept. 26
+% usage
+% [y,x]=SLE(n,chi)
+% input:
+% n = number of points (e.g. n=10000)
+% chi parameter of driving function (often: 0<chi<8 )
+% ouput:
+% [x,y] coordinates of stochastic planar curve with corresponding plot.
+% example: simulation of 10000 points Loop Erased Random 
+% Walk (LERW) with chi=2:
+% [y,x]=SLE(10000,2).
+% additional files:
+%          fz1.m   (first map)
+%          fz2.m   (second map)
+%---------------------------------------------------
+
+  f1=0;                 %initialization of the curve
+ for h=1:n
+   p=rand(1);
+   if p>0.5
+ f1=[0;fz1(f1,chi)];    % call of the first map if p<0.5
+   else
+ f1=[0;fz2(f1,chi)];    % else call of the second map 
+   end
+ end  
+ x=real(f1);            % x is the real part of the curve
+ y=imag(f1);            % y is the imaginary part of the curve
+
+ figure(1),
+ plot(x,y,'r-','LineWidth',1.0);
+ title(['planar curve for \chi = ' num2str(chi)])
+ axis equal
+ grid on
+
+end
